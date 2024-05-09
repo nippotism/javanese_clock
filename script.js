7c62ca1e597961daa8f3477fca6e36b34a29357a
Syariful Hanif <syarifulhanif@gmail.com>
2024-05-09 11:29:42 +0700

    fix logic

--- script.js
+++ script.js
@@ -174,7 +174,7 @@ function getNextPrayerTime(timings) {
         jamm = mintohourmin(distanceToNextPrayer)[0];
         menitt = mintohourmin(distanceToNextPrayer)[1];
         console.log('lolos '+((Math.floor(menitt/5))*5)+' '+menitt+' '+ jamm);
-        document.getElementById('mesej2').textContent = `${jamm<1? '':jam[jamm]+" jam"} ${menet[((Math.floor(menitt/10))*10)]<10? 'delok neh': menet[((Math.floor(menitt/10))*10)]+' menet'}  meneh solat ${nextPrayerName}.`;
+        document.getElementById('mesej2').textContent = `${jamm<1? '':jam[jamm]+" jam"} ${menet[((Math.floor(menitt/10))*10)]<10? jamm<10? 'delok':'': menet[((Math.floor(menitt/10))*10)]+' menet'}  meneh solat ${nextPrayerName}.`;
     }
 
 }
