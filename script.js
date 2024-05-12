b10ff6758da18c3dd0b35b4018ba2062d4d8c52e
Syariful Hanif <syarifulhanif@gmail.com>
2024-05-12 23:01:28 +0700

    fix logic

--- script.js
+++ script.js
@@ -190,7 +190,7 @@ function getNextPrayerTime(timings) {
 
         if(menitt<10){
             if(jamm<1){
-                document.getElementById('mesej2').textContent = `${menet[menitt]} menet neh solat ${nextPrayerName}.`;
+                document.getElementById('mesej2').textContent = `${menet[menitt]} menit neh solat ${nextPrayerName}.`;
             }else{
                 document.getElementById('mesej2').textContent = `${jamm<1? '':jam[jamm]+" jam"} ${menet[menitper10]} menit meneh solat ${nextPrayerName}.`;
             }   
